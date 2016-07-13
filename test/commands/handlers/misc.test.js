73d6d9440fc3e61ef8a41b3b229f15add0deabf2
Drew DeVault <sir@cmpwn.com>
2016-07-12 20:17:46 -0400

    Style

--- test/commands/handlers/misc.test.js
+++ test/commands/handlers/misc.test.js
@@ -10,28 +10,28 @@ chai.use(sinonChai);
 
 describe('src/commands/handlers/misc.js', function() {
 
-  describe('PING handler', function() {
+    describe('PING handler', function() {
 
-    it('should respond with the appropriate PONG message', function () {
-      var mock = mocks.IrcCommandHandler([misc]);
-      mock.handlers.PING(parse("PING example.com"));
-      expect(mock.spies.connection.write).to.have.been.calledOnce;
-      expect(mock.spies.connection.write).to.have.been.calledWith("PONG example.com");
+        it('should respond with the appropriate PONG message', function () {
+            var mock = mocks.IrcCommandHandler([misc]);
+            mock.handlers.PING(parse("PING example.com"));
+            expect(mock.spies.connection.write).to.have.been.calledOnce;
+            expect(mock.spies.connection.write).to.have.been.calledWith("PONG example.com");
+        });
+
     });
 
-  });
+    describe('PONG handler', function() {
 
-  describe('PONG handler', function() {
+        it('should respond with the appropriate PONG message', function () {
+            var mock = mocks.IrcCommandHandler([misc]);
+            mock.handlers.PONG(parse("PONG one.example.com two.example.com"));
+            expect(mock.spies.emit).to.have.been.calledOnce;
+            expect(mock.spies.emit).to.have.been.calledWith("pong", {
+                message: "two.example.com"
+            });
+        });
 
-    it('should respond with the appropriate PONG message', function () {
-      var mock = mocks.IrcCommandHandler([misc]);
-      mock.handlers.PONG(parse("PONG one.example.com two.example.com"));
-      expect(mock.spies.emit).to.have.been.calledOnce;
-      expect(mock.spies.emit).to.have.been.calledWith("pong", {
-        message: "two.example.com"
-      });
     });
-
-  });
 
 });
