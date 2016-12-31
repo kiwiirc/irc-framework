91a3650c2bf25c88e83438af00725f5655e49a47
Max Leiter <mleiter18@priorypanther.com>
2016-12-30 15:51:25 -1000

    parseMask tests

---
+++ test/helper.test.js
@@ -0,0 +1,47 @@
+"use strict";
+let chai = require('chai'),
+    Helper = require('../src/helper'),
+    expect = chai.expect;
+
+chai.use(require('chai-subset'));
+
+describe('src/irclineparser.js', function () {
+    describe('mask parsing', function () {
+
+        it('should recognize when just passed a nick', function () {
+            var msgObj = Helper.parseMask("something");
+
+            expect(msgObj).to.containSubset({
+                nick: "something",
+            });
+        });
+
+        it('should recognize when just passed a nick and user', function () {
+            var msgObj = Helper.parseMask("something!something");
+
+            expect(msgObj).to.containSubset({
+                nick: "something",
+                user: "something",
+            });
+        });
+
+        it('should recognize when just passed a user and host', function () {
+            var msgObj = Helper.parseMask("something@something");
+
+            expect(msgObj).to.containSubset({
+                host: "something",
+                user: "something",
+            });
+        });
+
+        it('should recognize when just passed a nick, user, and host', function () {
+            var msgObj = Helper.parseMask("something!something@something");
+
+            expect(msgObj).to.containSubset({
+                nick: "something",
+                host: "something",
+                user: "something",
+            });
+        });
+    });
+});
