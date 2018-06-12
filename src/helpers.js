e3bd1970b8702c73905b81383321a765bbad7bf3
Jason Papakostas <vithos@gmail.com>
2018-06-11 23:29:07 -0500

    fix message tag parsing: split value by = into at most 2 parts

--- src/helpers.js
+++ src/helpers.js
@@ -2,6 +2,7 @@
 
 var Helper = {
 	parseMask: parseMask,
+	splitOnce: splitOnce,
 };
 
 module.exports = Helper;
@@ -37,4 +38,37 @@ function parseMask(mask) {
 		user: user,
 		host: host,
 	};
+}
+
+function splitOnce(input, separator) {
+	if (typeof input !== 'string' || typeof separator !== 'string') {
+		throw new TypeError('input and separator must be strings');
+	}
+
+	let splitPos;
+
+	if (separator === '') {
+		// special handling required for empty string as separator
+
+		// cannot match '' at start, so start searching after first character
+		splitPos = input.indexOf(separator, 1);
+		if (splitPos === input.length) {
+			// cannot match '' at end, so if that's all we found, act like we found nothing
+			splitPos = -1;
+		}
+	} else {
+		// normal non-zero-length separator
+		splitPos = input.indexOf(separator);
+	}
+
+	// no separator found
+	if (splitPos < 0) {
+		return [input];
+	}
+
+	// the normal case: split around first instance of separator
+	return [
+		input.slice(0, splitPos),
+		input.slice(splitPos+separator.length),
+	];
 }
