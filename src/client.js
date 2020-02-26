24be97dbe2160a679e905d95b45c5461ab173b61
Darren <darren@darrenwhitlen.com>
2020-02-26 11:03:14 +0000

    split messages into lines before splitting on words when sending a message

--- src/client.js
+++ src/client.js
@@ -377,18 +377,24 @@ module.exports = class IrcClient extends EventEmitter {
     }
 
     sendMessage(commandName, target, message) {
-        var that = this;
+        let lines = message
+            .split(/\r\n|\n|\r/)
+            .filter(i=>i);
 
-        // Maximum length of target + message we can send to the IRC server is 500 characters
-        // but we need to leave extra room for the sender prefix so the entire message can
-        // be sent from the IRCd to the target without being truncated.
-        var blocks = [...lineBreak(message, { bytes: this.options.message_max_length, allowBreakingWords: true, allowBreakingGraphemes: true })];
+        lines.forEach(line => {
+            // Maximum length of target + message we can send to the IRC server is 500 characters
+            // but we need to leave extra room for the sender prefix so the entire message can
+            // be sent from the IRCd to the target without being truncated.
+            let blocks = [
+                ...lineBreak(line, {
+                    bytes: this.options.message_max_length,
+                    allowBreakingWords: true,
+                    allowBreakingGraphemes: true,
+                })
+            ];
 
-        blocks.forEach(function(block) {
-            that.raw(commandName, target, block);
+            blocks.forEach(block => this.raw(commandName, target, block));
         });
-
-        return blocks;
     }
 
     say(target, message) {
