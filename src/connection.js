6bddc374e865b26788c48be00f041e3f7c38bb5a
Frotty <Frotty@users.noreply.github.com>
2020-03-17 16:49:16 +0100

    Update connection.js

--- src/connection.js
+++ src/connection.js
@@ -146,7 +146,7 @@ module.exports = class Connection extends EventEmitter {
                     wait: reconnect_wait
                 });
                 
-                that.debugOut('Scheduling reconnect. Attempt: ' + that.reconnect_attempts + ' Wait: ' + reconnect_wait + 'ms');
+                that.debugOut('Scheduling reconnect. Attempt: ' + that.reconnect_attempts '/' + that.auto_reconnect_max_retries + ' Wait: ' + reconnect_wait + 'ms');
                 that.setTimeout(() => that.connect(), reconnect_wait);
             } else {
                 unbindTransportEvents(that.transport);
