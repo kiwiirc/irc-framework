d7b09c937f01a95ec048d6f1349e77458a1e9ab4
Pavel Djundik <xPaw@users.noreply.github.com>
2020-07-15 12:47:17 +0300

    Use native brotli compression
    Reverts 5a9dfc78389184f2c95b822bcfab0de63cec32b3
    
    Works on Node 10.16.0 and above.

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,43 @@
 // webpack v4
 const path = require('path');
 const CompressionPlugin = require('compression-webpack-plugin');
-const BrotliPlugin = require('brotli-webpack-plugin');
 
-const shouldCompress = /\.(js|css|html|svg)(\.map)?$/
+const shouldCompress = /\.(js|css|html|svg)(\.map)?$/;
 
 module.exports = {
-  mode: 'production',
-  entry: './dist/browser/src',
-  output: {
-    path: path.join(path.resolve(__dirname), 'dist', 'browser', 'static'),
-    filename: 'browser.js',
-    library: 'irc-framework',
-    libraryTarget: 'umd',
-    umdNamedDefine: true
-  },
-  module: {
-    rules: [ ]
-  },
-  plugins: [
-    new CompressionPlugin({
-      cache: false,
-      test: shouldCompress,
-    }),
-    new BrotliPlugin({
-      asset: '[path].br[query]',
-      test: shouldCompress,
-      threshold: 10240,
-      minRatio: 0.8,
-      deleteOriginalAssets: false,
-    }),
-  ],
-  optimization: {
-    minimize: true
-  },
-  devtool: 'source-map',
-  performance: {
-    assetFilter: assetFilename =>
-      !assetFilename.match(/\.map(\.(gz|br))?$/),
-  },
+    mode: 'production',
+    entry: './dist/browser/src',
+    output: {
+        path: path.join(path.resolve(__dirname), 'dist', 'browser', 'static'),
+        filename: 'browser.js',
+        library: 'irc-framework',
+        libraryTarget: 'umd',
+        umdNamedDefine: true
+    },
+    module: {
+        rules: []
+    },
+    plugins: [
+        new CompressionPlugin({
+            cache: false,
+            test: shouldCompress,
+        }),
+        new CompressionPlugin({
+            filename: '[path].br[query]',
+            algorithm: 'brotliCompress',
+            test: shouldCompress,
+            compressionOptions: { level: 11 },
+            threshold: 10240,
+            minRatio: 0.8,
+            deleteOriginalAssets: false
+        }),
+    ],
+    optimization: {
+        minimize: true
+    },
+    devtool: 'source-map',
+    performance: {
+        assetFilter: assetFilename =>
+            !assetFilename.match(/\.map(\.(gz|br))?$/),
+    },
 };
