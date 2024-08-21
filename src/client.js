f51e1d1d66926bd145165bacce09f780a186a974
ItsOnlyBinary <ItsOnlyBinary@users.noreply.github.com>
2024-08-21 15:18:10 +0100

    Make setTopic() remove the topic if newTopic is falsy (#367)
    * Make setTopic() remove the topic if newTopic is falsy
    
    * add clearTopic function
    
    * Improve documentation for setTopic

--- src/client.js
+++ src/client.js
@@ -611,7 +611,21 @@ module.exports = class IrcClient extends EventEmitter {
     }
 
     setTopic(channel, newTopic) {
+        if (!newTopic || !newTopic.trim()) {
+            // If newTopic is undefined or empty, remove the existing topic
+            // this check is to prevent unexpectedly requesting the current topic
+            // when trying to clear the topic
+            this.clearTopic(channel);
+            return;
+        }
+
         this.raw('TOPIC', channel, newTopic);
+    }
+
+    clearTopic(channel) {
+        // The trailing `:` is required otherwise it would be requesting the topic
+        // and not clearing it
+        this.raw(`TOPIC ${channel} :`);
     }
 
     ctcpRequest(target, type /*, paramN */) {
